cdd73e2ad7dc648d02df6b5320ed7ec1a2750870
Sai Shashank <104642011+Dharma-Raghava-Sai-Shashank@users.noreply.github.com>
2024-03-07 11:22:26 +0530

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI, {
+        const con = await mongoose.connect('mongodb+srv://21je0309:PKeKik6MM70s6Kxv@cluster0.a2j3bzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
